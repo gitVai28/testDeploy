b17adba8d66e523f3b8e6c43555b04bdc1747646
Vaishnavi <vaishnavichavan820@gmail.com>
2024-10-15 11:32:42 +0530

    initial commit 2

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,4 +1,4 @@
-const { signup, login } = require('../controllers/authController');
+const { signup, login } = require('../Controllers/AuthController');
 const { signupValidation, loginValidation } = require('../Middlewares/authValidation');
 
 const router = require('express').Router();
